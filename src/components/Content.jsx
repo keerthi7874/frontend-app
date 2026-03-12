4c37e5eb00094db2fc20a8aaa6332abd5da35dd5
keerthi7874 <katrothkeerthi@gmail.com>
2026-03-12 20:54:27 +0530

    updated

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./Content.css"
 
-const API_URL= import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL
 
 function Content() {
   // const [count, setCount] = useState(0);
